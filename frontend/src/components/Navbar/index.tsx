54d863cae3d9915e81db0ea57eb7a6d3c16a77f2
unknown <Wesleyscorrea@hotmail.com>
2022-05-10 20:49:07 -0300

    Pagination layout

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,22 +1,22 @@
-import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css'
 
-function Navbar() {
+function Navbar(): JSX.Element {
 
     return (
         <header>
-         <nav className="container">
-           <div className="dsmovie-nav-content">
-            <h1>DSMovie</h1>
-            <a href="https://github.com/WesleySCorrea">
-                <div className="dsmovie-contact-container">
-                <GithubIcon />
-                <p className="dsmovie-contact-link">/WesleySCorrea</p>
-            </div>
-            </a>
-            </div>
-        </nav>
-    </header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/WesleySCorrea">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/WesleySCorrea</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
     );
 }
 
